0301ff256dcdead3d45fa1a315ba08b5ccc1991d
queens kisivuli <queenskisivuli@gmail.com>
2023-05-24 14:13:25 +0300

    search bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Map from './componets/Map'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Map from "./components/Map";
+import SearchBar from "./components/SearchBar";
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div>
-     <Map />
+      <SearchBar />
+      <Map />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
