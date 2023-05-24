6f4680b963ed681f012097fb64486e2fde5ce7d9
queens kisivuli <queenskisivuli@gmail.com>
2023-05-24 22:47:37 +0300

    app context init

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,43 @@
-import { useState } from "react";
+import { useState, createContext } from "react";
 import "./App.css";
 import Map from "./components/Map";
 import SearchBar from "./components/SearchBar";
 import Result from "./components/Result";
+
+export const AppContext = createContext();
+
 function App() {
+  const [lng, setLng] = useState(3.3792);
+  const [lat, setLat] = useState(6.5244);
+  const [userIp, setUserIp] = useState("");
+  const [location, setLocation] = useState("");
+  const [timeZone, setTimeZone] = useState("");
+  const [isp, setISP] = useState("");
+
   return (
     <div>
-      <div className="search-area">
-        <SearchBar />
-        <Result
-          ipAddress="192.212.174.101"
-          location="Brooklyn, NY, 10001"
-          timeZone="utc-05:00"
-          isp="spacex starlink"
-        />
-      </div>
-      <Map />
+      <AppContext.Provider
+        value={{
+          lng,
+          setLng,
+          lat,
+          setLat,
+          userIp,
+          setUserIp,
+          location,
+          setLocation,
+          timeZone,
+          setTimeZone,
+          isp,
+          setISP,
+        }}
+      >
+        <div className="search-area">
+          <SearchBar />
+          <Result />
+        </div>
+        <Map />
+      </AppContext.Provider>
     </div>
   );
 }
