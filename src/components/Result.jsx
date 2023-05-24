11d7471ff82419b9724d069e4452e9373aeb708c
queens kisivuli <queenskisivuli@gmail.com>
2023-05-24 22:49:07 +0300

    show user location data

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,9 +1,13 @@
-const Result = ({ ipAddress, location, timeZone, isp }) => {
+import { useContext } from "react";
+import { AppContext } from "../App";
+
+const Result = () => {
+  const { userIp, location, timeZone, isp } = useContext(AppContext);
   return (
     <div className="result">
       <div className="result__box">
         <h5 className="result__box--title">ip address</h5>
-        <h3 className="result__box--result">{ipAddress}</h3>
+        <h3 className="result__box--result">{userIp}</h3>
       </div>
       <div className="result__box">
         <h5 className="result__box--title">location</h5>
